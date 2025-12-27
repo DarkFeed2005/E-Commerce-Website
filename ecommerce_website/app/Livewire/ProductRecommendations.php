c172f2e1c326ca2abc604a4a082c07b9e6291fe1
KPolitX <kalanayasassrisathruwan123@gmail.com>
2025-12-27 20:09:36 +0530

    Create ProductRecommendations.php

---
+++ ecommerce_website/app/Livewire/ProductRecommendations.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ProductRecommendations extends Component
+{
+    public function render()
+    {
+        return view('livewire.product-recommendations');
+    }
+}
